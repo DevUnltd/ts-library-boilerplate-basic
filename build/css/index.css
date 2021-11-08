eba2bb2ddc562f95f5694882f367a54337850fa9
Francisco Hodge <hello@franciscohodge.com>
2021-11-08 09:26:15 +0000

    Build update

--- build/css/index.css
+++ build/css/index.css
@@ -1,6 +1,6 @@
 /*!
  * 
- *   @hodgef/ts-library-boilerplate-basic v1.1.13
+ *   @hodgef/ts-library-boilerplate-basic v1.1.14
  *   https://github.com/hodgef/ts-library-boilerplate-basic
  *
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef) and project contributors.
